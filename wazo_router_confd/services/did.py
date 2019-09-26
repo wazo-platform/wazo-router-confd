0ab50b3e903d6daac7c997a568a3649206544651
Fabio Tranchitella <fabio@tranchitella.eu>
2019-09-26 14:08:29 +0200

    Enh: delete end-points

--- wazo_router_confd/services/did.py
+++ wazo_router_confd/services/did.py
@@ -1,6 +1,6 @@
 import re
 
-from typing import Optional
+from typing import List, Optional
 
 from sqlalchemy.orm import Session
 
@@ -11,19 +11,21 @@ from wazo_router_confd.schemas import did as schema
 re_did_prefix_from_regex = re.compile('[^0-9]')
 
 
-def get_did(db: Session, did_id: int):
+def get_did(db: Session, did_id: int) -> DID:
     return db.query(DID).filter(DID.id == did_id).first()
 
 
-def get_did_by_regex(db: Session, regex: Optional[str]):
+def get_did_by_regex(db: Session, regex: Optional[str]) -> DID:
     return db.query(DID).filter(DID.did_regex == regex).first()
 
 
-def get_dids(db: Session, skip: int = 0, limit: int = 100):
+def get_dids(db: Session, skip: int = 0, limit: int = 100) -> List[DID]:
     return db.query(DID).offset(skip).limit(limit).all()
 
 
-def get_dids_by_tenant_id(db: Session, tenant: int, skip: int = 0, limit: int = 100):
+def get_dids_by_tenant_id(
+    db: Session, tenant: int, skip: int = 0, limit: int = 100
+) -> List[DID]:
     return db.query(DID).filter(DID.tenant_id == tenant).offset(skip).limit(limit).all()
 
 
@@ -32,7 +34,7 @@ def get_did_prefix_from_regex(did_regex: str) -> str:
     return did_prefix
 
 
-def create_did(db: Session, did: schema.DIDCreate):
+def create_did(db: Session, did: schema.DIDCreate) -> DID:
     db_did = DID(
         did_regex=did.did_regex,
         did_prefix=get_did_prefix_from_regex(did.did_regex),
@@ -46,7 +48,7 @@ def create_did(db: Session, did: schema.DIDCreate):
     return db_did
 
 
-def update_did(db: Session, did_id: int, did: schema.DIDUpdate):
+def update_did(db: Session, did_id: int, did: schema.DIDUpdate) -> DID:
     db_did = db.query(DID).filter(DID.id == did_id).first()
     if db_did is not None:
         db_did.did_regex = (
@@ -63,4 +65,12 @@ def update_did(db: Session, did_id: int, did: schema.DIDUpdate):
         )
         db.commit()
         db.refresh(db_did)
+    return db_did
+
+
+def delete_did(db: Session, did_id: int) -> DID:
+    db_did = db.query(DID).filter(DID.id == did_id).first()
+    if db_did is not None:
+        db.delete(db_did)
+        db.commit()
     return db_did
--- wazo_router_confd/tests/test_api_dids.py
+++ wazo_router_confd/tests/test_api_dids.py
@@ -267,3 +267,56 @@ def test_update_did_not_found(app=None, client=None):
         },
     )
     assert response.status_code == 404
+
+
+@get_app_and_client
+def test_delete_did(app=None, client=None):
+    from wazo_router_confd.database import SessionLocal
+    from wazo_router_confd.models.carrier import Carrier
+    from wazo_router_confd.models.carrier_trunk import CarrierTrunk
+    from wazo_router_confd.models.domain import Domain
+    from wazo_router_confd.models.tenant import Tenant
+    from wazo_router_confd.models.ipbx import IPBX
+    from wazo_router_confd.models.did import DID
+
+    session = SessionLocal(bind=app.engine)
+    tenant = Tenant(name='fabio')
+    domain = Domain(domain='testdomain.com', tenant=tenant)
+    ipbx = IPBX(
+        tenant=tenant,
+        domain=domain,
+        customer=1,
+        ip_fqdn='mypbx.com',
+        registered=True,
+        username='user',
+        sha1='da39a3ee5e6b4b0d3255bfef95601890afd80709',
+        sha1b='f10e2821bbbea527ea02200352313bc059445190',
+    )
+    carrier = Carrier(name='carrier', tenant=tenant)
+    carrier_trunk = CarrierTrunk(
+        name='carrier_trunk1', carrier=carrier, sip_proxy='proxy.somedomain.com'
+    )
+    did = DID(
+        did_regex=r'^(\+?1)?(8(00|44|55|66|77|88)[2-9]\d{6})$',
+        tenant=tenant,
+        ipbx=ipbx,
+        carrier_trunk=carrier_trunk,
+    )
+    session.add_all([tenant, domain, ipbx, carrier, carrier_trunk, did])
+    session.commit()
+    #
+    response = client.delete("/dids/1")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 1,
+        "did_regex": r"^(\+?1)?(8(00|44|55|66|77|88)[2-9]\d{6})$",
+        "carrier_trunk_id": 1,
+        "tenant_id": 1,
+        "ipbx_id": 1,
+    }
+
+
+@get_app_and_client
+def test_delete_did_not_found(app=None, client=None):
+    response = client.delete("/dids/1")
+    assert response.status_code == 404
