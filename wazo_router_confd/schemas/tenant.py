6479221e5816ccc93f776561711a2894fe86d12e
Fabio Tranchitella <fabio@tranchitella.eu>
2019-10-24 13:35:17 +0200

    Enh: add uuid support to tenants

--- wazo_router_confd/schemas/tenant.py
+++ wazo_router_confd/schemas/tenant.py
@@ -2,11 +2,13 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from pydantic import BaseModel, constr
+from typing import Optional
 
 
 class Tenant(BaseModel):
     id: int
     name: constr(max_length=256)  # type: ignore
+    uuid: Optional[constr(max_length=32)]  # type: ignore
 
     class Config:
         orm_mode = True
@@ -14,6 +16,7 @@ class Tenant(BaseModel):
 
 class TenantCreate(BaseModel):
     name: str
+    uuid: Optional[constr(max_length=32)]  # type: ignore
 
 
 class TenantUpdate(BaseModel):
