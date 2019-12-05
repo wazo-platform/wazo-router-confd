2ace0c3bb78a26050e8d464b4a5df537ab977f1f
Mehdi Abaakouk <mabaakouk@wazo.io>
2019-12-05 14:48:41 +0100

    tests: fix database migration creation
    Since database creation have been done by session, the database
    migration was tested against an already created database.
    
    This change fix it to run it on a fresh one

--- wazo_router_confd/tests/test_database_migration.py
+++ wazo_router_confd/tests/test_database_migration.py
@@ -6,16 +6,19 @@ import pprint
 
 from alembic.migration import MigrationContext  # type: ignore
 from alembic.autogenerate import compare_metadata  # type: ignore
+from sqlalchemy.orm.session import close_all_sessions  # type: ignore
 
 from wazo_router_confd import database
+from wazo_router_confd import conftest
 from wazo_router_confd.models.base import Base
 
 
-def test_database_migration(database_uri):
+def test_database_migration():
+    database_uri = conftest.create_temporary_database()
     config = dict(database_uri=database_uri)
-
     app = mock.Mock()
     app = database.setup_database(app, config)
+
     try:
         database.upgrade_database(app, config, force_migration=True)
         with app.engine.begin() as conn:
@@ -23,4 +26,5 @@ def test_database_migration(database_uri):
             diff = compare_metadata(ctx, Base.metadata)
             assert diff == [], pprint.pformat(diff, indent=2, width=20)
     finally:
+        close_all_sessions()
         app.engine.dispose()
