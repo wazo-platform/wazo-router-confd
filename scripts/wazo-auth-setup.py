eb67ea7df4c981b76cfde6e1e3983f9d3a2991a7
Fabio Tranchitella <fabio@tranchitella.eu>
2020-02-11 12:46:38 +0100

    Fix: wazo-auth-setup.py

---
+++ scripts/wazo-auth-setup.py
@@ -0,0 +1,40 @@
+import requests
+
+
+response = requests.post(
+    "https://localhost:9497/api/auth/_set_token",
+    json={
+        "auth_id": "uuid-multitenant",
+        "metadata": {
+            "pbx_user_uuid": "uuid-multitenant",
+            "tenant_uuid": "ffffffff-ffff-4c1c-ad1c-ffffffffffff",
+            "uuid": "uuid-multitenant"
+        },
+        "token": "valid-token-multitenant"
+    },
+    verify=False,
+)
+assert response.status_code == 204
+
+response = requests.post(
+    "https://localhost:9497/api/auth/_set_tenants",
+    json=[
+        {
+            "name": "valid-tenant1",
+            "parent_uuid": "ffffffff-ffff-4c1c-ad1c-ffffffffffff",
+            "uuid": "ffffffff-ffff-4c1c-ad1c-ffffffffffff"
+        },
+        {
+            "name": "valid-tenant2",
+            "parent_uuid": "ffffffff-ffff-4c1c-ad1c-ffffffffffff",
+            "uuid": "ffffffff-ffff-4c1c-ad1c-eeeeeeeeeee2"
+        },
+        {
+            "name": "valid-tenant3",
+            "parent_uuid": "ffffffff-ffff-4c1c-ad1c-ffffffffffff",
+            "uuid": "ffffffff-ffff-4c1c-ad1c-eeeeeeeeeee3"
+        }
+    ],
+    verify=False,
+)
+assert response.status_code == 204
